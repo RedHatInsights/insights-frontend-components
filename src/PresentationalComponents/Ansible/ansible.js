25c3b768fae4d5c6a5fbd3bb4cde5d8cfbbc8cd3
Ryan Long <rlong@redhat.com>
2018-08-07 12:08:31 -0400

    removing name and changing default to supported

--- src/PresentationalComponents/Ansible/ansible.js
+++ src/PresentationalComponents/Ansible/ansible.js
@@ -3,7 +3,7 @@ import propTypes from 'prop-types';
 
 import classNames from 'classnames';
 
-import './ansible-icon.scss';
+import './ansible.scss';
 
 /**
  * This is a dumb component that only recieves properties from a smart component.
@@ -12,18 +12,18 @@ import './ansible-icon.scss';
  * @param props the props given by the smart component.
  */
 
-const AnsibleIcon = ({supported, className, ...props}) => {
+const Ansible = ({unsupported, className, ...props}) => {
 
     let ansibleLogoClass = classNames(
-        'AnsibleIcon',
-        { [`is-supported`]: supported },
-        { [`is-unsupported`]: !supported }
+        'Ansible',
+        { [`is-supported`]: !unsupported },
+        { [`is-unsupported`]: unsupported }
     );
 
-    let unsupported;
+    let unsupportedSlash;
     let ariaLabels = {};
-    if (!supported) {
-        unsupported =
+    if (unsupported) {
+        unsupportedSlash =
         <React.Fragment>
             <rect x="1245.1" y="272.4" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 3082.5815 -510.474)" className="st0" width="803.8" height="221.5"/>
             <rect x="-279.7" y="904" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 2450.9683 1014.3757)" className="st1" width="2590.2" height="221.5"/>
@@ -45,14 +45,14 @@ const AnsibleIcon = ({supported, className, ...props}) => {
                     C5.9,455.7,459.2,2.4,1018.3,2.4C1577.5,2.4,2030.8,455.7,2030.8,1014.8 M1035.4,620.9l262,646.6L901.7,955.8L1035.4,620.9
                     L1035.4,620.9z M1500.8,1416.5l-403-969.9c-11.5-28-34.5-42.8-62.4-42.8c-28,0-52.7,14.8-64.2,42.8L528.9,1510.4h151.3l175.1-438.6
                     l522.5,422.1c21,17,36.2,24.7,55.9,24.7c39.5,0,74-29.6,74-72.3C1507.7,1439.4,1505.3,1428.3,1500.8,1416.5L1500.8,1416.5z"/>
-                {unsupported}
+                {unsupportedSlash}
             </svg>
         </i>
     );
 };
 
-export default AnsibleIcon;
+export default Ansible;
 
-AnsibleIcon.propTypes = {
-    supported: propTypes.bool
+Ansible.propTypes = {
+    unsupported: propTypes.bool
 };
--- src/PresentationalComponents/AnsibleIcon/index.js
+++
@@ -1,1 +0,0 @@
-export { default as AnsibleIcon } from './ansible-icon';
