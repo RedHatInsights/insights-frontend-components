4c16983f31cdee67208e9a79bcde671b7e30ab37
Ryan Long <rlong@redhat.com>
2018-09-06 09:10:20 -0400

    adding pageheader and dark modes

--- src/PresentationalComponents/Main/Main.js
+++ src/PresentationalComponents/Main/Main.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import classNames from 'classnames';
 
 /**
  * This is a component that wraps the page
  */
 
-const Main = ({className, ...props}) => {
+const Main = ({className, children, dark, ...props}) => {
 
     let mainClasses = classNames(
         className,
-        'pf-l-page__main-section'
+        'pf-l-page__main-section',
+        { ['pf-m-dark']: dark }
     );
 
     return (
         <section { ...props } className={ mainClasses }>
-            {this.props.children}
+            { children }
         </section>
     );
 };
 
 export default Main;
 
-PageHeader.propTypes = {
-  children: propTypes.any.isRequired
+Main.propTypes = {
+    className: propTypes.string,
+    children: propTypes.any.isRequired,
+    dark: propTypes.bool
 };
