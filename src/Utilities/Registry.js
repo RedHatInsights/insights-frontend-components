1b3b3053065dfccea0d7c3bb6014a02b1b35c0ef
Ryan Long <rlong@redhat.com>
2018-09-20 12:39:21 -0400

    adding linting and fixing errors

--- src/Utilities/Registry.js
+++ src/Utilities/Registry.js
@@ -2,18 +2,19 @@ import ReducerRegistry from './ReducerRegistry';
 let registry;
 
 function init(initialState = {}, middleware = []) {
-  if(!registry) {
-    registry = new ReducerRegistry(initialState, [...middleware]);
-  }
-  return registry;
+    if (!registry) {
+        registry = new ReducerRegistry(initialState, [ ...middleware ]);
+    }
+
+    return registry;
 }
 
 export default function(settings = {}) {
-  return function(target) {
-    target.prototype.registry = init(settings.initialState, settings.middleware);
-  }
+    return function(target) {
+        target.prototype.registry = init(settings.initialState, settings.middleware);
+    };
 }
 
 export function getStoreFromRegistry(initialState = {}, middleware = []) {
-  return init(initialState, middleware).getStore();
+    return init(initialState, middleware).getStore();
 }
