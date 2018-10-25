109c9647fb5be0e487430d4e947b6cd812ea334f
Martin Maroši <marvusm.mmi@gmail.com>
2018-10-25 14:07:59 +0200

    Added missing form fields validators.

--- src/SmartComponents/Forms/Validators/validators.js
+++ src/SmartComponents/Forms/Validators/validators.js
@@ -10,14 +10,43 @@ const memoize = fn => {
     };
 };
 
-export const required = memoize(({ message } = { message: 'Required' }) => {
-    return value => value && value.length > 0 ? undefined : message;
+const stringValidator = memoize(({ message } = { message: 'Field value has to be string' }) =>
+    value => !value ? undefined : typeof value === 'string' ? undefined : message);
+const booleanValidator = memoize(({ message } = { message: 'Field value has to be boolean' }) =>
+    value => !value ? undefined : typeof value === 'boolean' ? undefined : value === 'true' || value === 'false' ? undefined : message);
+
+export const patternValidator = memoize(({
+    pattern = /^.*&/,
+    message = 'Value must match pattern',
+    showPattern = true
+} = {
+    pattern: /^.*&/,
+    message: 'Value must match pattern',
+    showPattern: true
+}) => {
+    let verifiedPattern = typeof pattern === 'string' ? new RegExp(pattern) : pattern;
+    return value =>
+        !value ? undefined : value.toString().match(verifiedPattern)
+            ? undefined
+            : `${message}${showPattern ? `: ${pattern.toString()}` : ''}`;
 });
 
-export const minLength = treshold =>
-    value =>
-        !value || value.length === 0
-            ? undefined
-            : value.length >= treshold
-                ? undefined
-                : `Should be atleast ${treshold} long`;
+export const dataTypeValidator = type => ({
+    string: options => stringValidator({ message: 'Field value has to be string', ...options }),
+    integer: options => patternValidator({ pattern: /^\d*$/, message: 'Value must be integer', ...options, showPattern: false }),
+    boolean: options => booleanValidator({ message: 'Field value has to be boolean', ...options }),
+    number: options => patternValidator({ pattern: /^\d*[.]{0,1}\d*$/, message: 'Values mut be number', ...options, showPattern: false })
+})[type];
+
+export const required = memoize(({ message } = { message: 'Required' }) => value => value && value.length > 0 ? undefined : message);
+
+export const minLength = memoize(({ treshold } = { treshold: 1 }) =>
+    value => !value ? undefined : value.length >= treshold
+        ? undefined
+        : `Should be atleast ${treshold} long`);
+
+export const minValue = memoize(({ message = 'Should be greater or equal to:', value } = { message: 'Should be greater or equal to:' }) =>
+    number => !number ? undefined : number >= value ? undefined : `${message} ${value}`);
+
+export const maxValue = memoize(({ message = 'Should be less or equal to:', value } = { message: 'Should be less or equal to:' }) =>
+    number => !number ? undefined : number <= value ? undefined : `${message} ${value}`);
--- src/SmartComponents/Forms/test/Validators/validators.test.js
+++ src/SmartComponents/Forms/test/Validators/validators.test.js
@@ -1,4 +1,4 @@
-import { required, minLength } from '../../Validators/validators';
+import { required, minLength, minValue, maxValue, dataTypeValidator, patternValidator } from '../../Validators/validators';
 
 describe('Form validators', () => {
   describe('required validator', () => {
@@ -17,11 +17,131 @@ describe('Form validators', () => {
 
   describe('min length validator', () => {
     it('should pass the validation', () => {
-      expect(minLength(5)('Some longer text')).toBeUndefined();
+      expect(minLength({ treshold: 5})('Some longer text')).toBeUndefined();
     })
 
     it('should return default error message', () => {
-      expect(minLength(99)('Foo')).toEqual('Should be atleast 99 long');
+      expect(minLength({ treshold: 99})('Foo')).toEqual('Should be atleast 99 long');
+    });
+  });
+
+  describe('min value validator', () => {
+    it('should pass the validation', () => {
+      expect(minValue({ value: 99 })(123)).toBeUndefined();
+    });
+
+    it('should pass the validation if no input given', () => {
+      expect(minValue({ value: 99 })()).toBeUndefined();
+    });
+  
+    it('should not pass the validation', () => {
+      expect(minValue({ value: 99 })(1)).toEqual('Should be greater or equal to: 99');
+    });
+  
+    it('should not pass the validation and return custom message', () => {
+      expect(minValue({ value: 99, message: 'Foo' })(1)).toEqual('Foo 99');
+    });
+
+    describe('max value validator', () => {
+      it('should pass the validation', () => {
+        expect(maxValue({ value: 99 })(1)).toBeUndefined();
+      });
+
+      it('should pass the validation if no input given', () => {
+        expect(maxValue({ value: 99 })()).toBeUndefined();
+      });
+
+      it('should not pass the validation', () => {
+        expect(maxValue({ value: 99 })(123)).toEqual('Should be less or equal to: 99');
+      });
+
+      it('should not pass the validation and return custom validation', () => {
+        expect(maxValue({ value: 99, message: 'Foo' })(123)).toEqual('Foo 99');
+      });
+    });
+  });
+
+  describe('pattern validator', () => {
+    it('should pass pattern validation with no configuration', () => {
+      expect(patternValidator('Foo')).toBeUndefined();
+    });
+
+    it('should pass pattern validation with configured regexp pattern', () => {
+      expect(patternValidator({ pattern: /^Foo$/ })('Foo')).toBeUndefined();
+    });
+
+    it('should pass pattern validation with configured error message', () => {
+      expect(patternValidator({ message: 'Regexp pattern' })()).toBeUndefined();
+    });
+
+    it('should fail pattern validation and return default message', () => {
+      expect(patternValidator({ pattern: /^Foo$/ })('Bar')).toEqual('Value must match pattern: /^Foo$/');
+    });
+
+    it('should fail pattern validation and return custom message', () => {
+      expect(patternValidator({ pattern: /^Foo$/, message: 'Custom message' })('Bar')).toEqual('Custom message: /^Foo$/');
+    });
+
+    it('should fail pattern validation and return custom message but not show pattern', () => {
+      expect(patternValidator({ pattern: /^Foo$/, message: 'Custom message', showPattern: false })('Bar')).toEqual('Custom message');
+    });
+
+    it('should pass pattern validation with configured regexp pattern as string', () => {
+      expect(patternValidator({ pattern: '^Foo$' })('Foo')).toBeUndefined();
+    });
+  });
+
+  describe('data type validator', () => {
+    it('should return string validator and pass', () => {
+      expect(dataTypeValidator('string')({ message: 'String message' })('Foo')).toBeUndefined()
+    });
+
+    it('should return string validator and pass if no value is given', () => {
+      expect(dataTypeValidator('string')({ message: 'String message' })()).toBeUndefined()
+    });
+
+    it('should return string validator and fail', () => {
+      expect(dataTypeValidator('string')({ message: 'Should be string' })(123)).toEqual('Should be string')
+    });
+
+    it('should return return integerValidator and pass', () => {
+      expect(dataTypeValidator('integer')()(123)).toBeUndefined()
+    });
+
+    it('should return return integerValidator and pass if no value given', () => {
+      expect(dataTypeValidator('integer')()()).toBeUndefined()
+    });
+
+    it('should return return integerValidator and fail', () => {
+      expect(dataTypeValidator('integer')({ message: 'Should be integer' })('Foo')).toEqual('Should be integer')
+    });
+
+    it('should return return integerValidator and fail if decimal nuber given', () => {
+      expect(dataTypeValidator('integer')({ message: 'Should be integer' })(123.456)).toEqual('Should be integer')
+    });
+    
+    it('should return boolean validator and pass', () => {
+      expect(dataTypeValidator('boolean')()(false)).toBeUndefined()
+    });
+    
+    it('should return boolean validator and pass if no value given', () => {
+      expect(dataTypeValidator('boolean')()()).toBeUndefined()
+    });
+    
+    it('should return boolean validator and pass fail', () => {
+      expect(dataTypeValidator('boolean')({ message: 'Value should be boolean' })('Foo')).toEqual('Value should be boolean')
+    });
+
+    it('should return return numberValidator and pass', () => {
+      expect(dataTypeValidator('number')()(123.33)).toBeUndefined()
+    });
+
+    it('should return return numberValidator and pass if no value given', () => {
+      expect(dataTypeValidator('number')()()).toBeUndefined()
+    });
+
+    it('should return return numberValidator and fail', () => {
+      expect(dataTypeValidator('integer')({ message: 'Should be number' })('Foo')).toEqual('Should be number')
     });
   });
 });
