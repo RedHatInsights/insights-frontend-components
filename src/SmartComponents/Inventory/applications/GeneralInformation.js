97215e93fd345ea37b513e131c4ccdc280c88ba5
Karel Hala <khala@redhat.com>
2019-01-09 18:51:29 +0100

    Move general info components to cards

--- src/SmartComponents/Inventory/applications/GeneralInformation.js
+++ src/SmartComponents/Inventory/applications/GeneralInformation.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { TextContent, Text, TextVariants, Grid, GridItem, GutterSize } from '@patternfly/react-core';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    TextContent,
+    Text,
+    TextVariants,
+    Grid,
+    GridItem,
+    GutterSize
+} from '@patternfly/react-core';
 import { Table } from '../../../PresentationalComponents/Table';
 import { Pagination } from '../../../PresentationalComponents/Pagination';
 import './general-information.scss';
 
-class GeneralInformation extends React.Component {
+class GeneralInformation extends Component {
     render() {
         const { entity } = this.props;
         const facts = entity.facts.qpc || entity.facts.inventory;
@@ -15,51 +25,63 @@ class GeneralInformation extends React.Component {
             <Grid sm={ 6 } md={ 6 } lg={ 6 } gutter={ GutterSize.md }>
                 <GridItem>
                     <TextContent>
-                        <div>
-                            <Text component={ TextVariants.h1 }>System</Text>
-                        </div>
-                        <div>
-                            <span>Manufacturer:</span>
-                            <span>{ facts.virtualized_type || 'Unknown' }</span>
-                        </div>
-                        <div>
-                            <span>Release:</span>
-                            <span>{ facts.os_release || 'Unknown' }</span>
-                        </div>
-                        <div>
-                            <span>Server Type:</span>
-                            <span>{ facts.infrastructure_type || 'Unknown' }</span>
-                        </div>
-                        <div>
-                            <span>Time Zone:</span>
-                            <span>{ timeZone && `${timeZone.slice(0, 3)}:${timeZone.slice(3)}` }</span>
-                        </div>
+                        <Card>
+                            <CardHeader>
+                                <Text component={TextVariants.h1}>System</Text>
+                            </CardHeader>
+                            <CardBody>
+                                <div>
+                                    <span>Manufacturer:</span>
+                                    <span>{facts.virtualized_type || 'Unknown'}</span>
+                                </div>
+                                <div>
+                                    <span>Release:</span>
+                                    <span>{facts.os_release || 'Unknown'}</span>
+                                </div>
+                                <div>
+                                    <span>Server Type:</span>
+                                    <span>{facts.infrastructure_type || 'Unknown'}</span>
+                                </div>
+                                <div>
+                                    <span>Time Zone:</span>
+                                    <span>{timeZone && `${timeZone.slice(0, 3)}:${timeZone.slice(3)}`}</span>
+                                </div>
+                            </CardBody>
+                        </Card>
                     </TextContent>
                 </GridItem>
                 <GridItem>
                     <TextContent>
-                        <div>
-                            <Text component={ TextVariants.h1 }>Bios</Text>
-                        </div>
-                        <div>
-                            <span>UUID:</span>
-                            <span>{ entity.bios_uuid || 'Unknown' }</span>
-                        </div>
+                        <Card>
+                            <CardHeader>
+                                <Text component={TextVariants.h1}>Bios</Text>    
+                            </CardHeader>
+                            <CardBody>
+                                <div>
+                                    <span>UUID:</span>
+                                    <span>{entity.bios_uuid || 'Unknown'}</span>
+                                </div>
+                            </CardBody>
+                        </Card>
                     </TextContent>
                 </GridItem>
                 <GridItem span={ 12 }>
                     <TextContent>
-                        <div>
-                            <Text component={ TextVariants.h1 }>Network</Text>
-                        </div>
-                        <Table rows={ [] }
-                            header={ [
-                                { title: 'Process Name', hasSort: false },
-                                { title: 'IP Address', hasSort: false },
-                                { title: 'Port', hasSort: false }
-                            ] }
-                            footer={ <Pagination numberOfItems={ 0 } /> }
-                        />
+                        <Card>
+                            <CardHeader>
+                                <Text component={TextVariants.h1}>Network</Text>
+                            </CardHeader>
+                            <CardBody>
+                                <Table rows={[]}
+                                    header={[
+                                        { title: 'Process Name', hasSort: false },
+                                        { title: 'IP Address', hasSort: false },
+                                        { title: 'Port', hasSort: false }
+                                    ]}
+                                    footer={<Pagination numberOfItems={0} />}
+                                />
+                            </CardBody>
+                        </Card>
                     </TextContent>
                 </GridItem>
             </Grid>
