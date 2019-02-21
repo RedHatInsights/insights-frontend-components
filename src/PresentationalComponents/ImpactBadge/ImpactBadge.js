cff6e8e2a6729f5554fd9d9c6dd7588d1441a2ff
Jan Elznic <jelznic@redhat.com>
2019-02-21 14:49:30 +0100

    Impact badge - Props updated, fixed hasTooltip condition, consts file added

--- src/PresentationalComponents/ImpactBadge/ImpactBadge.js
+++ src/PresentationalComponents/ImpactBadge/ImpactBadge.js
@@ -2,56 +2,22 @@ import { QuestionIcon, SecurityIcon } from '@patternfly/react-icons';
 import { Tooltip } from '@patternfly/react-core';
 import propTypes from 'prop-types';
 import React from 'react';
+import { impactList, colorList } from './consts';
 
 class ImpactBadge extends React.Component {
     constructor(props) {
         super(props);
-
-        this.colorList = {
-            default: 'var(--pf-global--Color--200)', // grey
-            danger: 'var(--pf-global--danger-color--100)',
-            warning: 'var(--pf-global--warning-color--100)',
-            orange: '#ec7a08' // orange
-        };
-
-        /* I'm not shure that the list of impacts is ordered correctly by relevancy */
-        this.impactList = {
-            Critical: {
-                title: 'Critical',
-                color: this.colorList.danger
-            },
-            High: {
-                title: 'High',
-                color: this.colorList.orange
-            },
-            Important: {
-                title: 'Important',
-                color: this.colorList.orange
-            },
-            Medium: {
-                title: 'Medium',
-                color: this.colorList.warning
-            },
-            Moderate: {
-                title: 'Moderate',
-                color: this.colorList.warning
-            },
-            Low: {
-                title: 'Low',
-                color: this.colorList.default
-            }
-        };
     }
 
     getColoredBadgeByImpact() {
-        if (this.impactList.hasOwnProperty(this.props.impact)) {
+        if (impactList.hasOwnProperty(this.props.impact)) {
             return {
-                icon: <SecurityIcon size={ this.props.size } color={ this.impactList[this.props.impact].color } />,
-                title: this.impactList[this.props.impact].title
+                icon: <SecurityIcon size={ this.props.size } color={ impactList[this.props.impact].color } />,
+                title: impactList[this.props.impact].title
             };
         } else {
             return {
-                icon: <QuestionIcon size={ this.props.size } color={ this.colorList.default } />,
+                icon: <QuestionIcon size={ this.props.size } color={ colorList.default } />,
                 title: 'Unknown'
             };
         }
@@ -61,11 +27,16 @@ class ImpactBadge extends React.Component {
         const badge = this.getColoredBadgeByImpact(this.props.impact);
         return (
             <React.Fragment>
-                <div>
-                    <Tooltip position={this.props.tooltip_position} content={ <div>Impact: { badge.title }</div> }>
+                { this.props.hasTooltip === true ? (
+                    <Tooltip
+                        position={ this.props.tooltipPosition }
+                        content={ <div>{ this.props.tooltipPrefix + badge.title }</div> }
+                    >
                         { badge.icon }
                     </Tooltip>
-                </div>
+                ) : (
+                    <span>{ badge.icon }</span>
+                ) }
             </React.Fragment>
         );
     }
@@ -73,15 +44,17 @@ class ImpactBadge extends React.Component {
 
 ImpactBadge.defaultProps = {
     impact: 'N/A',
-    hastooltip: false,
-    tooltip_position: 'right',
+    hasTooltip: false,
+    tooltipPosition: 'right',
+    tooltipPrefix: '',
     size: 'md'
 };
 
 ImpactBadge.propTypes = {
     impact: propTypes.string,
-    hastooltip: propTypes.bool,
-    tooltip_position: propTypes.string,
+    hasTooltip: propTypes.bool,
+    tooltipPosition: propTypes.string,
+    tooltipPrefix: propTypes.string,
     size: propTypes.string // sm, md, lg and xl
 };
 
