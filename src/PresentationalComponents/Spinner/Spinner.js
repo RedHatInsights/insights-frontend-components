2e837b16aaa296f65f0321965efe3b1c81703217
Ryan Long <rlong@redhat.com>
2019-03-25 11:56:29 -0400

    adding spinner component <?.x.?

---
+++ src/PresentationalComponents/Spinner/Spinner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './spinner.scss';
+
+const Spinner = ({ centered, className, ...props }) => {
+
+    let spinnerClasses = classNames(
+        'ins-c-spinner',
+        { [`ins-m-center`]: centered },
+        className
+    );
+
+    return (
+        <span className={spinnerClasses} {...props} />
+    );
+};
+
+Spinner.propTypes = {
+    centered: PropTypes.bool,
+    className: PropTypes.string
+};
+
+
+export default Spinner;
